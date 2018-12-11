f568948a23b28e68254f8d450b2381a36bad9c44
Wil.ly Hellcat <newellwm@gmail.com>
2018-12-11 12:09:13 -0500

    rearrange imports

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express')
 const mongoose = require('mongoose')
+const express = require('express')
 const session = require('express-session')
 const passport = require('passport')
 
+// initialize database and authentication
 const keys = require('./config/keys')
 mongoose.connect(keys.mLabURI, { useNewUrlParser: true })
 require('./models/User')
-
-const app = express()
 require('./services/passport')
 
+// initialize express with sessions
+const app = express()
 app.use(
 	session({
 		secret: [keys.cookieSecret],
@@ -21,6 +22,7 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
+// initialize routes and configure server
 require('./routes/authRoutes')(app)
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
