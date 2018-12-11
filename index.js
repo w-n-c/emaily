7f02bf16a47f1cff59ec7d15650244d07cf163f1
Wil.ly Hellcat <newellwm@gmail.com>
2018-12-11 09:23:27 -0500

    wire up cookie based sessions

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express')
-const keys = require('./config/keys')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const passport = require('passport')
 
+const keys = require('./config/keys')
 mongoose.connect(keys.mLabURI, { useNewUrlParser: true })
-require('./models/User') // must be called before passport require (to load schema)
+require('./models/User')
 
 const app = express()
 require('./services/passport')
-require('./routes/authRoutes')(app)
 
+app.use(
+	session({
+		secret: [keys.cookieSecret],
+		maxAge: 14 * 24 * 60 * 60 * 1000,
+		resave: false,
+		saveUninitialized: false
+	})
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
