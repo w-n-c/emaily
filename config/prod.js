b7070718f2250267fceb696e4671fac247027835
Wil.ly Hellcat <newellwm@gmail.com>
2018-12-11 12:10:28 -0500

    configure keys to handle dev and production environments

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientId: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mLabURI: process.env.M_LAB_URI
+	cookieSecret: process.env.COOKIE_SECRET
+}
