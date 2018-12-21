de61d74d17da3757859df6828676cede2177d694
Wil.ly Hellcat <newellwm@gmail.com>
2018-12-21 07:25:39 -0500

    node served static files in production

--- index.js
+++ index.js
@@ -26,5 +26,16 @@ app.use(passport.session())
 // initialize routes and configure server
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('cliend/build'))
+
+	// we are using react-router. Direct all unknown urls to index.html
+	const path = require('path')
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
