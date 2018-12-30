e308459282fa30032056b6b7a776371cd56f3964
Wil.ly Hellcat <newellwm@gmail.com>
2018-12-29 20:53:02 -0500

    fix breaking typo

--- models/Recipient.js
+++ models/Recipient.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 
-const recipientSchema = newSchema({
+const recipientSchema = new Schema({
 	email: String,
 	responded: { type: Boolean, default: false }
 })
